b9f3f61d75fa27eb4a926234045424dfd19a4476
Jeson <204138282@qq.com>
2017-07-17 17:19:42 +0800

    Initial Commit

---
+++ JsAndOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JsAndOS
+//
+//  Created by Jeson on 17/07/2017.
+//  Copyright © 2017 Jeson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
